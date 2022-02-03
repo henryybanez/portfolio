7da2e0d48394b83395313a96d342d43d2b304f07
henryybanez <98938934+henryybanez@users.noreply.github.com>
2022-02-03 23:00:52 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            show: false,
+            project: {
+                title: 'Portfolio',
+                name: 'Henry Ybañez',
+                email: 'htybanez1974@gmail.com'
+            },
+            pictures: [
+                { src: 'pictures/img001.jpg', show: false },
+                { src: 'pictures/img002.jpg', show: false },
+                { src: 'pictures/img003.jpg', show: false },
+                { src: 'pictures/img004.jpg', show: false },
+                { src: 'pictures/img005.jpg', show: false },
+                { src: 'pictures/img006.jpg', show: false },
+                { src: 'pictures/img009.jpg', show: false },
+                { src: 'pictures/img010.jpg', show: false },
+                { src: 'pictures/img011.jpg', show: false },
+                { src: 'pictures/img012.jpg', show: false },
+                { src: 'pictures/img013.jpg', show: false },
+                { src: 'pictures/img014.jpg', show: false },
+                { src: 'pictures/img015.jpg', show: false },
+                { src: 'pictures/img016.png', show: false },
+                { src: 'pictures/img008.jpg', show: false },
+                { src: 'pictures/img017.jpg', show: false },
+                { src: 'pictures/img023.jpg', show: false },
+                { src: 'pictures/img019.jpg', show: false },
+                { src: 'pictures/img018.jpg', show: false },
+                { src: 'pictures/img020.jpg', show: false },
+                { src: 'pictures/img021.jpg', show: false },
+                { src: 'pictures/img024.jpg', show: false },
+                { src: 'pictures/img025.jpg', show: false },
+                { src: 'pictures/img026.jpg', show: false },
+                { src: 'pictures/img027.jpg', show: false },
+                { src: 'pictures/img028.jpg', show: false },
+                { src: 'pictures/img022.jpg', show: false },
+                { src: 'pictures/img031.jpg', show: false },
+                { src: 'pictures/img029.jpg', show: false },
+                { src: 'pictures/img30.jpg', show: false },
+                { src: 'pictures/img032.jpg', show: false },
+                { src: 'pictures/img033.jpg', show: false },
+                { src: 'pictures/img1.jpg', show: false },
+                { src: 'pictures/img2.jpg', show: false },
+                { src: 'pictures/img4.jpg', show: false },
+                { src: 'pictures/img5.jpg', show: false },
+                { src: 'pictures/img7.jpg', show: false },
+                { src: 'pictures/img6.jpg', show: false },
+                { src: 'pictures/img8.jpg', show: false },
+                { src: 'pictures/img3.jpg', show: false },
+                { src: 'pictures/img9.jpg', show: false },
+                { src: 'pictures/img10.jpg', show: false },
+                { src: 'pictures/img12.jpg', show: false },
+                { src: 'pictures/img13.jpg', show: false },
+                { src: 'pictures/img14.jpg', show: false },
+                { src: 'pictures/img15.jpg', show: false },
+                { src: 'pictures/img16.jpg', show: false },
+                { src: 'pictures/img17.jpg', show: false },
+            ]
+        }
+    }
+})
+
+app.mount('#app')
